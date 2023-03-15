705c79bd17e90ff50d88caace8054dfdf75d7e96
Yuki San <hikikomori.yukisan@gmail.com>
2023-03-15 22:10:22 +0700

    feat: add select keyword

---
+++ test/select_channel.go
@@ -0,0 +1,48 @@
+package test
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func SelectChannel() {
+	runtime.GOMAXPROCS(2)
+
+	numbers := []int{3, 4, 3, 5, 6, 7, 8, 9, 3, 2}
+	fmt.Println("numbers :", numbers)
+	ch1 := make(chan float64)
+	go getAvarage(numbers, ch1)
+
+	ch2 := make(chan int)
+	go getMax(numbers, ch2)
+
+	for i := 0; i < 2; i++ {
+		select {
+		case avg := <-ch1:
+			fmt.Printf("Avg \t: %.2f \n", avg)
+
+		case max := <-ch2:
+			fmt.Printf("Max \t: %d \n", max)
+		}
+	}
+}
+
+func getAvarage(numbers []int, ch chan float64) {
+	sum := 0
+	for _, e := range numbers {
+		sum += e
+	}
+
+	ch <- float64(sum) / float64(len(numbers))
+}
+
+func getMax(numbers []int, ch chan int) {
+	max := numbers[0]
+	for _, e := range numbers {
+		if max < e {
+			max = e
+
+		}
+	}
+	ch <- max
+}
