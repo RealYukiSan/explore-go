ea25da6a92185339b793f93a3e58d647a54f89d6
Yuki San <hikikomori.yukisan@gmail.com>
2023-03-16 21:27:45 +0700

    feat: add another case of struct

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Learn and explore golang programming language
 
 # Explanation? go to [wiki pages](https://github.com/Reyuki-san/explore-go/wiki)
+
+# Symbol yang tertera wiki
+- (?) | berarti belum terkonfirmasi / belum yakin atas statement tersebut.
--- test/data_type/struct.go
+++ test/data_type/struct.go
@@ -2,23 +2,46 @@ package test
 
 import "fmt"
 
+type person struct {
+	name string
+	age  uint8
+}
+
 type student struct {
-	name  string
+	person
 	grade int
 }
 
 func ImplementStruct() {
-	var s1 student
+	var s1 = student{}
 	s1.grade = 1
 	s1.name = "John Titor"
+	s1.age = 23
 
-	var s2 = student{name: "kyoma"}
+	fmt.Println("name :", s1.name)
+	fmt.Println("age :", s1.age)
+	fmt.Println("grade :", s1.grade)
+	// ini berguna jika parent dan child memiliki property yang sama, assign valuenya perlu dilakukan secara eksplisit!
+	fmt.Println("another way to access child prop / sub struct :", s1.person.name)
 
-	var s3 = student{"kurisu", 3}
+	var p2 = person{"kami da", 18}
+	var s2 = student{p2, 2}
+	fmt.Println("it's also works :", s2)
 
-	fmt.Println("student 1 :", s1.name)
-	fmt.Println("student 2 :", s2.name)
-	fmt.Println("student 3 :", s3.name)
+	// anonymous struct!
+	var s3 = struct {
+		person
+		grade int
+	}{person{"third person", 19}, 3}
 
-	fmt.Println("zero value of s2 :", s2.grade)
+	fmt.Println("this is anonymous struct, useful if the struct only used once :", s3)
+
+	// kombinasi antara slice dan struct, note: this combination also works with anonymous type, like the example above
+	var allStudents = []student{
+		s1, s2, s3,
+	}
+
+	for _, student := range allStudents {
+		fmt.Println(student.name, "age is", student.age)
+	}
 }
