13a6cc09253d1f0204f5fe546d3bbcc1182370a5
Yuki San <hikikomori.yukisan@gmail.com>
2023-03-17 06:51:56 +0700

    feat: add slice type

--- test/data_type/array.go
+++ test/data_type/array.go
@@ -1,6 +1,9 @@
 package test
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func ImplementArray() {
 	var names [4]string
@@ -25,9 +28,31 @@ func ImplementArray() {
 	numbers := [...]int{1, 2, 3, 4}
 
 	fmt.Println("data array \t:", numbers)
+	fmt.Println("type \t:", reflect.TypeOf(numbers).Kind())
 
 	// multi-dimension array
 	matrix := [2][3]int{{3, 2, 1}, {1, 2, 3}}
 
 	fmt.Println("matrix :", matrix)
+}
+
+func ImplementSlice() {
+	var fruits = make([]string, 2)
+	fruits[0] = "khuldi"
+	fruits[1] = "sirsak"
+	fmt.Println("type \t:", reflect.TypeOf(fruits).Kind())
+
+	fruits = []string{"apple", "grape", "banana"}
+	aFruits := fruits[:3]
+	// index terakhir pada slice ini menandakan jumlah kapasitas yang ditentukan
+	bFruits := fruits[2:3:3]
+	cFruits := fruits[2:]
+	dFruits := aFruits[:]
+
+	cFruits[0] = "changed"
+	fmt.Println(fruits)
+	fmt.Println(aFruits)
+	fmt.Println(bFruits, len(bFruits), cap(bFruits))
+	fmt.Println(cFruits)
+	fmt.Println(dFruits)
 }
